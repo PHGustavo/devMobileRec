2139a1a31324b8edf9fa7e5314663a25ef843847
PHGustavo <guhoepers25@gmail.com>
2024-08-28 15:05:59 -0300

    feat: adicionado link nos menus direcionando a "categorias"

--- src/components/templates/medium/MediumAside.vue
+++ src/components/templates/medium/MediumAside.vue
@@ -1,57 +1,50 @@
 <script setup>
-import LogoTitle from '@/components/templates/LogoTitle.vue';
+import LogoTitle from '@/components/templates/LogoTitle.vue'
 </script>
 <template>
-    <div class="logo_and_menu">
-        <logo-title class="mb-2" />
-        <div class="divider" />
-        <div class="menu">
-            <router-link to="/">
-                <i class="icon mdi mdi-home-outline" /> Home
-            </router-link>
-        </div>
-        <div class="divider" />
-        <div class="menu">
-            <router-link to="/">
-                <i class="icon mdi mdi-account-circle-outline" /> Perfil
-            </router-link>
-            <router-link to="/">
-                <i class="icon mdi mdi-cart-outline" /> Carrinho
-            </router-link>
-            <router-link to="/login">
-                <i class="icon mdi mdi-account" /> Login
-            </router-link>
-        </div>
+  <div class="logo_and_menu">
+    <logo-title class="mb-2" />
+    <div class="divider" />
+    <div class="menu">
+      <router-link to="/"> <i class="icon mdi mdi-home-outline" /> Home </router-link>
+      <router-link to="/categorias"> <i class="icon mdi mdi-list-box" /> Categorias </router-link>
+
     </div>
-    <logo-title />
+    <div class="divider" />
+    <div class="menu">
+      <router-link to="/"> <i class="icon mdi mdi-account-circle-outline" /> Perfil </router-link>
+      <router-link to="/"> <i class="icon mdi mdi-cart-outline" /> Carrinho </router-link>
+      <router-link to="/login"> <i class="icon mdi mdi-account" /> Login </router-link>
+    </div>
+  </div>
+  <logo-title />
 </template>
 <style scoped>
 .mb-2 {
-    margin-bottom: 1.5rem;
+  margin-bottom: 1.5rem;
 }
 
 .icon {
-    font-size: 2rem;
-    align-self: center;
-
+  font-size: 2rem;
+  align-self: center;
 }
 
 .divider {
-    margin-top: 1.rem;
-    border-top: 1px solid #EEEEEE;
+  margin-top: 1rem;
+  border-top: 1px solid #eeeeee;
 }
 
 .menu {
-    padding: 3rem;
+  padding: 3rem;
 }
 
 .menu a {
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    color: #000000;
-    gap: 1rem;
-    font-size: 1.3rem;
-    margin-top: 2.2rem;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: #000000;
+  gap: 1rem;
+  font-size: 1.3rem;
+  margin-top: 2.2rem;
 }
 </style>
